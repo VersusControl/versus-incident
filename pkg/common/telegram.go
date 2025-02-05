92e2a152a415b90ecd472c80685d4ee917d06074
Nghĩa Huỳnh Lê Nhất <nghiahln@techhaus.vn>
2025-02-05 16:41:29 +0700

    fix: remove redundant comments in code

--- pkg/common/telegram.go
+++ pkg/common/telegram.go
@@ -33,28 +33,19 @@ func NewTelegramProvider(cfg TelegramConfig) *TelegramProvider {
 }
 
 func (t *TelegramProvider) SendAlert(i *m.Incident) error {
-	fmt.Printf("Template Path: %s\n", t.templatePath)
-	fmt.Printf("Bot Token: %s\n", t.botToken)
-	fmt.Printf("Chat ID: %s\n", t.chatID)
-	fmt.Printf("Content: %+v\n", i.Content)
-
 	funcMap := template.FuncMap{
 		"replaceAll": strings.ReplaceAll,
 	}
 
 	tmpl, err := template.New(filepath.Base(t.templatePath)).Funcs(funcMap).ParseFiles(t.templatePath)
 	if err != nil {
-		fmt.Printf("Template error: %v\n", err)
 		return fmt.Errorf("failed to parse template: %w", err)
 	}
 
 	var message bytes.Buffer
 	if err := tmpl.Execute(&message, i.Content); err != nil {
-		fmt.Printf("Template execute error: %v\n", err)
 		return fmt.Errorf("failed to execute template: %w", err)
 	}
-
-	fmt.Printf("Message: %s\n", message.String())
 
 	telegramMsg := TelegramMessage{
 		ChatID:    t.chatID,
@@ -64,21 +55,18 @@ func (t *TelegramProvider) SendAlert(i *m.Incident) error {
 
 	jsonData, err := json.Marshal(telegramMsg)
 	if err != nil {
-		fmt.Printf("JSON marshal error: %v\n", err)
 		return fmt.Errorf("failed to marshal message: %w", err)
 	}
 
 	url := fmt.Sprintf("https://api.telegram.org/bot%s/sendMessage", t.botToken)
 	resp, err := http.Post(url, "application/json", bytes.NewBuffer(jsonData))
 	if err != nil {
-		fmt.Printf("HTTP error: %v\n", err)
 		return fmt.Errorf("failed to send message: %w", err)
 	}
 	defer resp.Body.Close()
 
 	// Read and print response body
 	body, _ := io.ReadAll(resp.Body)
-	fmt.Printf("Response: %s\n", string(body))
 
 	if resp.StatusCode != http.StatusOK {
 		return fmt.Errorf("telegram API returned non-200 status code: %d, body: %s", resp.StatusCode, string(body))
