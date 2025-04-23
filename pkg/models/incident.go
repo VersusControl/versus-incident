549d18e020876c50384174aa82f17aa0768be11d
Quan Huynh <hoalongnatsu@gmail.com>
2025-04-23 15:55:33 +0700

    (feature): add handle resolve messages

--- pkg/models/incident.go
+++ pkg/models/incident.go
@@ -3,17 +3,17 @@ package models
 import "github.com/google/uuid"
 
 type Incident struct {
-	ID           string      `json:"id"`
-	TeamID       string      `json:"team_id"`
-	Content      interface{} `json:"content"`
-	Acknowledged bool        `json:"acknowledged"`
+	ID       string                  `json:"id"`
+	TeamID   string                  `json:"team_id"`
+	Content  *map[string]interface{} `json:"content"`
+	Resolved bool
 }
 
-func NewIncident(teamID string, content interface{}) *Incident {
+func NewIncident(teamID string, content *map[string]interface{}, resolved bool) *Incident {
 	return &Incident{
-		ID:           uuid.NewString(), // Generate a new UUID as a string
-		TeamID:       teamID,
-		Content:      content,
-		Acknowledged: false, // Default to not acknowledged
+		ID:       uuid.NewString(), // Generate a new UUID as a string
+		TeamID:   teamID,
+		Content:  content,
+		Resolved: resolved,
 	}
 }
